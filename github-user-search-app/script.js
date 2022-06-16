9a7832ed184cdfeeb13b5123e127374335419db1
tekac <soehnerj@gmail.com>
2022-06-16 18:53:09 -0400

    starting basic mobile first styling

---
+++ github-user-search-app/script.js
@@ -0,0 +1,16 @@
+import { Octokit } from "/node_modules@octokit/rest";
+
+const octokit = new Octokit({
+  auth: "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",
+});
+
+await octokit
+  .request("GET /users/{username}/hovercard", {
+    username: "TEKAC",
+  })
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {});
+
+// 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
