5ce5f65fd06ee7a71aadb64aba6ee930d3304f26
tekac <soehnerj@gmail.com>
2022-08-08 21:18:02 -0400

    starting container

--- stats_preview_card_component/tailwind.config.js
+++ stats_preview_card_component/tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    colors: {
+      mainback: "hsl(233, 47%, 7%)",
+      cardback: "hsl(244, 38%, 16%)",
+      accent: "hsl(277,64%,61%)",
+
+      primary: "hsl(0,0%,100%)",
+      secondary: "hsla(0,0%,100%,.75)",
+      tertiary: "hsla(0,0%,100%,.6)",
+    },
+    extend: {
+      fontFamily: {
+        inter: ["inter", "sans-serif"],
+        lexend: ["lexend Deca", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
